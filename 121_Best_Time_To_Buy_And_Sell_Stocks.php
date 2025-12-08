068e756b50e30336e3e4a7267592a7a5418f96d6
Ben Fisher <BenLfish00@gmail.com>
2025-12-08 10:29:44 -0500

    LeetCode Problem 121
    121. Best Time to Buy and Sell Stocks.
    PHP solution

---
+++ 121_Best_Time_To_Buy_And_Sell_Stocks.php
@@ -0,0 +1,20 @@
+class 121_Best_Time_To_Buy_And_Sell_Stocks {
+
+    /**
+     * @param Integer[] $prices
+     * @return Integer
+     */
+
+    function maxProfit($prices) {
+        $min = $prices[0];
+        $profit = 0;
+
+        foreach ($prices as $price) {
+            $min = min($min, $price);
+            $profit = max($profit, $price - $min);
+        }
+
+        // bcsub($price, $min)
+        return $profit;
+    }
+}
