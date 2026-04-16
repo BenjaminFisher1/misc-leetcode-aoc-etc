5835edcbd96bc0f44e6e81fb83e95fcf556fd1ce
Ben Fisher <BenLfish00@gmail.com>
2026-04-16 18:58:42 -0400

    125. Valid Palindrome java solution

---
+++ LeetCode/java/Valid_Palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String ns = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        System.out.println(ns);
+        char[] ca = ns.toCharArray();
+        int i = 0;
+        int j = ca.length - 1;
+
+        while ( i < j ){
+            if(ca[i] != ca[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+
+        return true;
+    }
+}
+
+//125. Valid Palindrome
